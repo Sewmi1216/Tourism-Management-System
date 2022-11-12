53f8608e3b7fa7355ba8e697b348a6207818772f
udari417 <108745710+udari417@users.noreply.github.com>
2022-11-12 11:48:17 +0530

    Entre_login UI

---
+++ Entrepreneur/Registration.php
@@ -0,0 +1,77 @@
+<html>
+    <head>
+        <title>Registration page</title>
+        <link rel="stylesheet" type="text/css" href="rstyle.css">
+        <body>
+        <div class="header">
+            <a href="#"><img src="Travel and Tourism Logo.png" alt="Company logo"></a>
+            <div class="header-right">
+            <a class="active" href="#home">Home</a>
+    
+  </div>
+  
+  <form action="/action_page.php">
+    <div class="container">
+        <h1>ENTREPRENURE REGISTRATION</h1>
+        
+
+        <label for="email"><b>BUSSINESS NAME</b></label>
+        <input type="text" placeholder="Enter Bussiness Name" >
+        <h4>CONTACT ENTREPRENURE DETAILS</h4>
+
+        <label for="name"><b>NAME</b></label>
+        <input type="text" placeholder="Enter Name" >
+
+        <label for="NIC"><b>NIC</b></label>
+        <input type="text" placeholder="Enter NIC" >
+
+        <label for="dob"><b>DOB</b></label>
+        <input type="text" placeholder="Enter DOB" >
+
+        <label for="username"><b>USERNAME</b></label>
+        <input type="text" placeholder="Enter Username" >
+
+        <label for="pwd"><b>PASSWORD</b></label>
+        <input type="text" placeholder="Enter Password" >
+
+        <label for="Phone"><b>CONTACT NUMBER</b></label>
+        <input type="text" placeholder="Enter Phone" >
+
+        <label for="Email"><b>EMAIL ADDRESS</b></label>
+        <input type="text" placeholder="Enter Email" >
+        
+        <h4>BUSSINESS DETAILS</h4>
+        <label for="Address"><b>ADDRESS</b></label>
+        <input type="text" placeholder="Enter Address" >
+
+        <label for="Email"><b>EMAIL ADDRESS</b></label>
+        <input type="text" placeholder="Enter Email" >
+
+        <label for="Phone"><b>CONTACT NUMBER</b></label>
+        <input type="text" placeholder="Enter Phone"  >
+
+        <label for="Profile"><b>PROFILE IMAGE</b></label><br>
+        <input type="file"  id="myFile" name="filename"><br><br>
+
+        <label for="Doc"><b>LEGAL DOCUMENT</b></label><br>
+        <input type="file"  id="myFile" name="filename">
+        
+
+
+        <hr>
+        
+
+        <button type="submit" class="registerbtn">Register</button>
+    </div>
+  
+   
+    <p>Already have an account? <a href="#">Sign in</a></p>
+  
+</form>
+</div>
+
+
+        
+        </body>
+    </head>
+</html>
---
+++ Entrepreneur/Untitled-1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Dashboard</title>
+        <link rel="stylesheet" type="text/css" href="rstyle.css">
+        <body>
+            <div class="header">
+                <a href="#"><img src="Travel and Tourism Logo.png" alt="Company logo"></a>
+                <div class="header-right">
+                <a class="active" href="#home">Home</a>
+        
+            </div>
+        </body>
+    </head>
+</html>
+
+  
