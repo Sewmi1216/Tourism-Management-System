c597adf58cd6583bac553385372b63eb28a173bb
Sewmi <sewmithimaya248@gmail.com>
2022-11-15 09:02:28 +0530

    nav bar updated

---
+++ Hotel/view/hotelPkg.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+     <link rel="stylesheet" href="../css/nav.css?v=<?php echo time(); ?>">
+      <link rel="stylesheet" href="../css/header.css?v=<?php echo time(); ?>">
+     <link href="../libs/fontawesome/css/fontawesome.css" rel="stylesheet">
+     <link href="../libs/fontawesome/css/brands.css" rel="stylesheet">
+     <link href="../libs/fontawesome/css/solid.css" rel="stylesheet">
+    </head>
+<body>
+    <?php include "nav.php"?>
+
+    <section class="home-section">
+     <?php include "dashboardHeader.php"?>
+      <div class="text">Hotel Packages</div>
+      <a href="addPkg.php"><i class="fa-regular fa-square-plus" style="font-size:30px;margin-left:950px;"></i></a>
+            
+    </section>
+
+</body>
+</html>
