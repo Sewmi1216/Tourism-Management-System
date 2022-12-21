6ca42b03f4ffb7356013d25a7e5510ba3eba4250
udari417 <108745710+udari417@users.noreply.github.com>
2022-12-21 23:16:14 +0530

    Udari-updated

--- view-entrepreneur/order.php
+++ view-entrepreneur/order.php
@@ -1,12 +1,4 @@
-<?php
-session_start();
-$user = "";
-if (isset($_SESSION["username"]) && isset($_SESSION["entID"])) {
-    $id = $_SESSION["entID"];
-} else {
-    header("location:Login.php");
-}
-?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 
@@ -25,20 +17,54 @@ if (isset($_SESSION["username"]) && isset($_SESSION["entID"])) {
 
     <section class="home-section">
         <?php include "dashboardHeader.php"?>
-        <div class="text">Craft Orders</div>
+        <div class="se" style="margin-top: 20px;">
+            <div class="searchSec">
+                <div class="page-title">Craft Orders </div>
+                <div class="input-container">
+                    <input class="input-field" type="text" placeholder="Search for products" name="search">
+                    <a href="" class="searchimg"><i class="fa fa-search icon"></i></a>
+                </div>
+                <button type="submit" class="btns">View All</button>
+                
+            </div>
+
+        </div>
         <div class="bg">
-        
-        <input type="text" id="search" onkeyup="myFunction()" placeholder="Search for names.."
-                title="Type in a name" style="width:1000px;margin-bottom:30px;">
-        <div>
-            <table>
-                <tr class="heading tblrw">
-                    <th class="tblh">Order ID</th>
-                    <th class="tblh">Order Date</th>
-                    <th class="tblh">Status</th>
-                    <th class="tblh">View</th>
-                    
-                </tr><?php
+                <table>
+                    <tr class="subtext tblrw">
+                        <th class="tblh">Order ID</th>
+                        <th class="tblh">Order Date</th>
+                        <th class="tblh">Status</th>
+                        <th class="tblh">view </th>
+                    </tr>
+                    <tr class="subtext tblrw">
+                        <td class="tbld">0001</td>
+                        <td class="tbld">2022/10/22</td>
+                        <td class="tbld">Accepted</td>
+                        <td class="tbld"><i class="fa-sharp fa-solid fa-bars art"
+                                    style="color:black;"></i></td>
+                        
+                        
+                    </tr>
+                    <tr class="subtext tblrw">
+                        <td class="tbld">0001</td>
+                        <td class="tbld">2022/10/22</td>
+                        <td class="tbld">Accepted</td>
+                        <td class="tbld"><i class="fa-sharp fa-solid fa-bars art"
+                                    style="color:black;"></i></td>
+                        
+                        
+                    </tr>
+                    <tr class="subtext tblrw">
+                        <td class="tbld">0001</td>
+                        <td class="tbld">2022/10/22</td>
+                        <td class="tbld">Accepted</td>
+                        <td class="tbld"><i class="fa-sharp fa-solid fa-bars art"
+                                    style="color:black;"></i></td>
+                        
+                        
+                    </tr>
+           <?php
 include "../controller/productController.php";
 $productcont = new productController();
 $res = $productcont->viewAll();
