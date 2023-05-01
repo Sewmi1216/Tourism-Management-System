8409780a6ff69f29bac0f5f196127b40450c2967
Sewmi <sewmithimaya248@gmail.com>
2023-05-02 03:10:09 +0530

    tour booking completed

---
+++ api/tourbooking.php
@@ -0,0 +1,38 @@
+<?php
+include "stripeconfig.php";
+include '../controller/touristController.php';
+
+$touristID = $_POST['tid'];
+$packageId = $_POST['pid'];
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$guests = $_POST['traverler'];
+
+$aDate = $_POST['adate'];
+$dDate = $_POST['ddate'];
+
+$token = $_POST["stripeToken"];
+$contact_name = $_POST["name"];
+$token_card_type = $_POST["stripeTokenType"];
+$phone = $_POST["phone"];
+// $email = $_POST["stripeEmail"];
+$total_amount = $_POST["tot_amount"];
+
+$charge = \Stripe\Charge::create([
+    "amount" => str_replace(",", "", $total_amount) * 100,
+    "currency" => 'usd',
+    "description" => 'TourBooking',
+    "source" => $token,
+]);
+
+$booking = new touristController();
+$booking->insertTourBooking($name, $phone, $email, $total_amount, $aDate, $dDate, $guests, $touristID, $packageId);
+
+if ($charge && $booking) {
+    echo "
+             <script>alert('Your tourbooking is successful');
+        window.location.href = '../view-tourist/tourpackagelist.php';
+        </script>";
+}
